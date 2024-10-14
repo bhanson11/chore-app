f3188892df6e0ce8df2f619c51120fd306f3ac8e
Britten Hanson <britten.hanson11@gmail.com>
2024-10-14 10:36:44 -0600

    creation of models for database tables and app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db
+
+app = Flask(__name__)
+
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///chores_app'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
+##########################################################################################
+## Parent User sign up / login / logout 
+
---
+++ models.py
@@ -0,0 +1,96 @@
+"""SQLAlchemy models for chores app"""
+
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+class Parent(db.Model):
+    """creating parent profile"""
+
+    __tablename__ = 'parents'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    username = db.Column(
+        db.Text, 
+        nullable=False,
+        unique=True
+    )
+
+    email = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True
+    )
+
+    password_hash = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    parent_id = db.Column(
+        db.integer,
+        nullable=True        
+    )
+
+class Child(db.Model):
+    """creating child profile - only parent can create a child profile"""
+
+    __tablename__ = 'children'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    username = db.Column(
+        db.Text, 
+        nullable=False,
+        unique=True
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False
+    )
+    
+class Chore(db.Model):
+    """creating chores"""
+
+    __tablename__ = 'chores'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    name = db.Column(
+        db.Text
+        nullable=False
+    )
+
+    category = db.Column(
+        db.Integer,
+        db.ForeignKey('categories.id', ondelete="cascade"),
+        nullable=False,
+    )
+
+class Category(db.Model):
+    """categories for chores"""
+
+    __tablename__ = 'categories'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    name = db.Column(
+        db.Text,
+        nullable=False
+    )
